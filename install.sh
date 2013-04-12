f211aa825c32ab28d70564df87a3f21234871192
luoi <hoanghh.vnu@gmail.com>
2013-04-12 19:13:55 +0700

    edit install.sh

--- install.sh
+++ install.sh
@@ -1,27 +1,26 @@
-# Written by: Luoi ST
 # Date create: Dec 12, 2012
 # Last modified: Apr 12, 2013 16:48
 BASH_PROFILE="$HOME/.bash_profile"
 [ -f $HOME/.bashrc ] && BASH_PROFILE="$HOME/.bashrc"
+chmod +x wmp3.sh
 if [ "root" == `whoami` ]
 then
     BIN_DIR="/usr/local/bin"
-    cp wgetvnmusic.sh "${BIN_DIR}/wgetvnmusic"
-    echo "Success installed \"wgetvnmusic\" for all user!"
-    echo "Run \"wgetvnmusic\" to print help (without quote)"
+    cp wmp3.sh "${BIN_DIR}/wmp3"
+    echo "Success installed \"wmp3\" for all user!"
+    echo "Run \"wmp3\" to print help (without quote)"
 else
     BIN_DIR="$HOME/.local/bin"
     if [ -d ${BIN_DIR} ]; then
-        cp wgetvnmusic.sh "${BIN_DIR}/wgetvnmusic"
+        cp wmp3.sh "${BIN_DIR}/wmp3"
     else
         mkdir -p ${BIN_DIR}
-        cp wgetvnmusic.sh "${BIN_DIR}/wgetvnmusic"
+        cp wmp3.sh "${BIN_DIR}/wmp3"
     fi
     echo "export PATH=$PATH:${BIN_DIR}" >> ${BASH_PROFILE}
-    echo "Success installed \"wgetvnmusic\" for only you!"
+    echo "Success installed \"wmp3\" for only you!"
     echo "Must logout or reboot your system to use them!"
-    echo "After, Run \"wgetvnmusic\" to print help (without quote)"
+    echo "After, Run \"wmp3\" to print help (without quote)"
     echo "TIP: Run script \"install\" file as root user to \
-use \"wgetvnmusic\" immediate!"
-
+use \"wmp3\" immediate!"
 fi
